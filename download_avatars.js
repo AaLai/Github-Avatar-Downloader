1e05664fc0930c9bf049c66c0859d43d0ea811e9
aaron <aaron_lai@hotmail.com>
2018-10-29 23:43:34 +0000

    Cleaned up download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -6,6 +6,9 @@ const fs = require('fs');
 // --- Bringing in user input
 const args = process.argv.slice(2);
 
+// --- Welcome message
+console.log('Welcome to the GitHub Avatar Downloader!');
+
 // --- Check that both fields are filled in user input
 function repoOwnerChecker () {
   return args[0];
@@ -22,14 +25,8 @@ function arguementChecker() {
   }
 }
 
-// --- Welcome message
-console.log('Welcome to the GitHub Avatar Downloader!');
-
 // --- Sending the HTTPS request to GitHub based on user input
 function getRepoContributors(repoOwner, repoName, cb) {
-  if (!repoOwner || !repoName) {
-    return console.log("You need to specify both a Repo Owner and Repo Name!")
-  }
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -46,24 +43,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-
-
-// --- Function checks user input, goes to entered URL and downloads pictures in local avatar folder
-function GitHubAvatarDownload () {
-  arguementChecker();
-  const repoOwner = args[0];
-  const repoName = args[1];
-  getRepoContributors(repoOwner, repoName, function(err, result) {
-      for (var num of result) {
-        downloadImageByURL(num.avatar_url, `avatars/${num.login}.jpg`)
-      };
-    if (err === !null) {
-      console.log("Errors:", err);
-    }
-    console.log("Result: Program finished");
-  });
-}
-
 // --- Requests and pipes image from URL to filePath
 function downloadImageByURL(url, filePath) {
 const location = String(url);
@@ -74,5 +53,21 @@ request.get(location)
        .pipe(fs.createWriteStream(filePath));
 }
 
-//-- Putting it all together
+// --- Function checks user input, goes to entered URL and downloads all avatars to local avatar folder will give error if avatar folder doesn't exist
+function GitHubAvatarDownload () {
+  arguementChecker();
+  const repoOwner = args[0];
+  const repoName = args[1];
+  getRepoContributors(repoOwner, repoName, function(err, result) {
+      for (var num of result) {
+        downloadImageByURL(num.avatar_url, `avatars/${num.login}.jpg`);
+      }
+    if (err === !null) {
+      console.log("Errors:", err);
+    }
+    console.log("Result: Program finished");
+  });
+}
+
+//-- Running
 GitHubAvatarDownload();
